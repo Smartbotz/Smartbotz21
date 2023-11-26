79bfe6ad28d108485788bcc0854b4859d945ff19
Smartbotz <fadlisuratinoyo12@gmail.com>
2023-11-26 10:18:38 +0700

    Update owner-tobc.js

--- plugins/owner-tobc.js
+++ plugins/owner-tobc.js
@@ -5,9 +5,12 @@ let handler = async (m, { conn, text }) => {
   let cc = conn.serializeM(text ? m : m.quoted ? await m.getQuotedObj() : false || m)
   let teks = text ? text : cc.text
   conn.reply(m.chat, `✅ Transmisi dibuat *Total:* ${chats.length} chats`, m)
-  for (let id of chats) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast|tx/i.test(teks) ? teks : `*BROADCAST┃ OWNER*\n_____________________\n ${teks} ` ), true).catch(_ => _)
+  for (let id of chats) {
+await sleep(2000) 
+ conn.copyNForward(id, cc, /bc|broadcast|tx/i.test(teks) ? teks : `*BROADCAST┃ OWNER*\n_____________________\n ${teks} ` ).catch(_ => _)
+ }
   m.reply('✅ Sukses broadcast ke semua chat :)')
-}
+  }
 handler.help = ['tobc >reply media<']
 handler.tags = ['owner']
 handler.command = /^(tobc|tx)$/i
@@ -17,5 +20,9 @@ module.exports = handler
 
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 const randomID = length => randomBytes(Math.ceil(length * .5)).toString('hex').slice(0, length)

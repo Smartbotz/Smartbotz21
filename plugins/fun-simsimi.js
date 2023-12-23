5e2ad51d558eed65a014a0780f940fb221a35892
Smartbotz <fadlisuratinoyo12@gmail.com>
2023-12-23 14:59:32 +0700

    Update fun-simsimi.js

--- plugins/fun-simsimi.js
+++ plugins/fun-simsimi.js
@@ -1,8 +1,12 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
+  try {
   let res = await fetch(`https://api.botcahx.live/api/search/simsimi?query=${encodeURIComponent(text)}&apikey=${btc}`)
   let json = await res.json()
   m.reply(json.result)
+  } catch {
+  m.reply('Terjadi kesalahan')
+  }
 }
 handler.help = ['simi', 'simsimi', 'simih'].map(v => v + ' <teks>')
 handler.tags = ['fun']

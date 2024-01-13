1cc540bf44317e530e98a4578308fedfd3f4ba0f
Smartbotz <fadlisuratinoyo12@gmail.com>
2024-01-13 18:01:49 +0700

    Update downloader-pindl.js

--- plugins/downloader-pindl.js
+++ plugins/downloader-pindl.js
@@ -4,6 +4,9 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!args[0]) {
     throw `Masukkan URL!\n\ncontoh:\n${usedPrefix}${command} https://pin.it/4CVodSq`;
   }
+  if (!args[0].match(/pin/gi)) {
+    throw `URL Tidak Ditemukan!`;
+  }
   try {
     m.reply(wait);
     const api = await fetch(`https://api.botcahx.eu.org/api/download/pinterest?url=${args[0]}&apikey=${btc}`);

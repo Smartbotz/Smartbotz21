843bbb34c2bcc76f748d69a3666769add7afe30d
Smartbotz <fadlisuratinoyo12@gmail.com>
2024-01-21 15:59:43 +0700

    Etika2

--- plugins/manggilataup.js
+++ plugins/manggilataup.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, usedPrefix: _p }) => {
   const text = m.text.toLowerCase();
 
-  if (text === 'p' || text === 'pe' || text === 'woy' || text === 'cuk' || text === 'coy' || text === 'cuy' || text === 'cok' || text === 'woi' || text === '🗿' || text === 'tol') {
+  if (text === 'p' || text === 'pe' || text === 'woy' || text === 'cuk' || text === 'coy' || text === 'cuy' || text === 'cok' || text === 'woi' || text === 'tol') {
     let info = `Harap yang sopan dalam memanggil orang, minimal mengatakan "Assalamu'alaikum warrahmatullahi Wabarakatuh" atau kalian bisa panggil Namanya. Tidak sopan itu, Jika *anda lebih tua* anda mengerti yang namanya etika.\n\n`;
 
     let greetings = {
@@ -42,7 +42,7 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
 };
 
 // UNTUK handler.customPrefix TAMBAHIN YANG KALIAN MAU YA DAN JANGAN LUPA JUGA DIGANTI PADA BAGIAN if text
-handler.customPrefix = /^(kakak|p|om|om xnuvers|om sora|pe|woy|cuk|cok|coy|woi|cuy|bapak|pak|ibu|bu|min|admin|🗿|tol)$/i;
+handler.customPrefix = /^(kakak|p|om|om xnuvers|om sora|pe|woy|cuk|cok|coy|woi|cuy|bapak|pak|ibu|bu|min|admin|tol)$/i;
 handler.command = new RegExp();
 
 module.exports = handler

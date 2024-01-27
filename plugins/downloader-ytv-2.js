9530d09059390e7d8265950b15eeda6eff23f8ee
Smartbotz <fadlisuratinoyo12@gmail.com>
2024-01-27 13:20:35 +0700

    Update ytv2

--- plugins/downloader-ytv-2.js
+++ plugins/downloader-ytv-2.js
@@ -1,11 +1,14 @@
 const youtube = require("yt-search");
+let ytdl = require('ytdl-core');
 let handler = async (m, { conn, text }) => {
   if (!text) throw 'Url nya mana?'
+  const isValid = await ytdl.validateURL(text);
+    if (!isValid) {
+        return m.reply("*your link not suported.*");
+    }
   m.reply('_Proses..._')
-  var search = await youtube(text);
-  var convert = search.videos[0];
-  let url = `https://aemt.me/youtube?url=${convert.url}&filter=audioandvideo&quality=highestvideo&contenttype=video/mp4`
-  conn.sendMessage(m.chat, { video: { url: url }, mimetype: 'video/mp4' }, { quoted: m })
+  let url = `https://aemt.me/youtube?url=${text}&filter=audioandvideo&quality=highestvideo&contenttype=video/mp4`
+  conn.sendFile(m.chat, url, null, `*Youtube Downloader*`, m)
 }
 handler.command = handler.help = ['ytv2']
 handler.tags = ['downloader']
